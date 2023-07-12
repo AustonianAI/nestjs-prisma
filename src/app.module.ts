53b23198e8093f53220ed80d85d7253193585040
Austin Johnson <austin@austinjohnson.me>
2023-07-12 14:10:35 -0500

    tutorial complete - prisma integrated with nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TweetsModule } from './modules/tweets/tweets.module';
+import { ApiModule } from './api/api.module';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TweetsModule, ApiModule],
   controllers: [],
-  providers: [],
+  providers: [AppService],
 })
 export class AppModule {}
